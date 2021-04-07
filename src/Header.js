fcc871965ea444d4d62d5c7c77d74a94f635735c
Caroline Lynch <carolineablynch@gmail.com>
2021-04-06 20:26:03 -0400

    creating header.js file

---
+++ src/Header.js
@@ -0,0 +1,9 @@
+nes (31 sloc)  1.06 KB
+  
+import { Link } from "react-router-dom";
+import { Navbar, Nav } from "react-bootstrap";
+
+const styles = {
+  link: { marginLeft: 10, marginRight: 10, color: "black" }
+};
+
