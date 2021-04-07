d9bcbc6f1bb9697eb617b7e54b25d29a4a6af644
Caroline Lynch <carolineablynch@gmail.com>
2021-04-06 20:58:14 -0400

    creating about page

---
+++ src/pages/About.js
@@ -0,0 +1,11 @@
+import { Jumbotron, Row, Col } from "react-bootstrap";
+import {
+  FaMailBulk,
+  FaPhoneSquareAlt,
+  FaLinkedin,
+  FaInstagramSquare,
+  FaGithubSquare,
+  FaPaperclip
+} from "react-icons/fa";
+import Fade from "react-reveal/Fade";
+import Layout from "../Layout";
